9958adefcfd072952f7eeb086e7d7600dd1da47a
Hasnain Raza <hasnainraza3899@gmail.com>
2024-01-21 18:13:18 +0500

    create weather web app

---
+++ script.js
@@ -0,0 +1,64 @@
+
+let APIKEY = "71859caaf52c42eb76c5c1251d9c7a43";
+let APIURL = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+let weatherIcon = document.querySelector(".weather-icon");
+
+// weather api
+const weatherCondition = async (city) => {
+    try {
+        const response = await fetch(APIURL + city + `&appid=${APIKEY}`)
+        const data = await response.json()
+        console.log(data)
+        document.querySelector(".condition").innerHTML = data.weather[0].main;
+        document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
+        document.querySelector(".city").innerHTML = data.name;
+        document.querySelector(".humidity-icon").innerHTML = '<img class="w-10" src="/assets/icons/humidity.png">';
+        document.querySelector(".humidity").innerHTML = Math.round(data.main.humidity) + "%";
+        document.querySelector(".hum-name").innerHTML = "Humidity"
+        document.querySelector(".wind-icon").innerHTML = ' <img class="w-10" src="/assets/icons/wind.png">';
+        document.querySelector(".wind").innerHTML = Math.round(data.wind.speed) + " km/h";
+        document.querySelector(".wind-name").innerHTML = "Wind Speed";
+
+
+        if (data.weather[0].main == "Clear") {
+            weatherIcon.src = "/assets/icons/clear.png";
+        }
+        else if (data.weather[0].main == "Clouds") {
+            weatherIcon.src = "/assets/icons/clouds.png"
+        }
+        else if (data.weather[0].main == "Drizzle") {
+            weatherIcon.src = "/assets/icons/deizzle.png"
+        }
+        else if (data.weather[0].main == "Fog") {
+            weatherIcon.src = "/assets/icons/fog.png"
+        }
+        else if (data.weather[0].main == "Mist") {
+            weatherIcon.src = "/assets/icons/mist.png"
+        }
+        else if (data.weather[0].main == "Rain") {
+            weatherIcon.src = "/assets/icons/rain.png"
+        }
+        else if (data.weather[0].main == "Smoke") {
+            weatherIcon.src = "/assets/icons/smoke.png"
+        }
+        else if (data.weather[0].main == "Snow") {
+            weatherIcon.src = "/assets/icons/snow.png"
+        } else {
+            weatherIcon.src = "/assets/icons/clear.png"
+        }
+
+
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+// search button function
+const Search = () => {
+    let inputValue = document.getElementById("input").value;
+    if (inputValue === "") {
+        return alert("Please Enter Your City Name");
+    }
+    weatherCondition(inputValue);
+    return document.getElementById("input").value = "";
+}
